75b012a9ba9dd3aafdcec60d69c847663171058f
ahmedrao567 <ahmedikram567@hmail.com>
2026-03-27 11:54:26 +0500

    again comminting for automation

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Hi this ahmed running a flask app hello. now i chaneg the code for trigeering teh automtion </h1>"
+    return "<h1>Hi this ahmed running a flask app hello. now i chaneg the code for trigeering teh automtion  gaindhdjaj</h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000) 
