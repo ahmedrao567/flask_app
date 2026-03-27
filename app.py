740db4ddd60e4edccadc5a08252e167be6319baf
ahmedrao567 <ahmedikram567@hmail.com>
2026-03-27 17:52:38 +0500

    automation

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>hello this is ahmed deploying the app on rocky server...</h1>"
+    return "<h1>Hello! This is Ahmed deploying the app on Rocky server...</h1>"
+
+# Add this webhook route
+@app.route("/github_webhook", methods=["POST"])
+def github_webhook():
+    data = request.json
+    print("Webhook payload received:", data)
+    return jsonify({"status": "success"}), 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5050) 
+    app.run(host="0.0.0.0", port=5050)
