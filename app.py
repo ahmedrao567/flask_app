2982b4114e8ba613c2f46f753eae81a5bc1008a5
ahmedrao567 <ahmedikram567@hmail.com>
2026-04-06 15:33:52 +0500

    automation with kubernetes1

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def home():
 
 
-    return "<h1>hello this is ahmed deploying the app on rocky server going to push it.=..= 31 MARCH 2026 kubernetes checking now with automation </h1>"
+    return "<h1>hello this is ahmed deploying the app on rockyy server going to push it.=..= 31 MARCH 2026 kubernetes checking now with automation </h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050) 
